06bac992586f0b170ba66c402137beaff2b3b62e
dongjihui <dongjihui0320@gmail.com>
2020-04-02 09:36:27 +0800

    董集辉第一次修改

--- teamtest/src/tste01.java
+++ teamtest/src/tste01.java
@@ -2,6 +2,7 @@ public class tste01 {
     public static void main(String[] args) {
         System.out.println("大家好我是一号队员");
         System.out.println("大家好我是陈文华~~~~");
+        System.out.println("d大家好我是董霁辉");
     }
 
 }
