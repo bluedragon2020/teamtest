ca527b329d320c81de7eb94a7933511d2075863b
sophia <sophiasaww@gmail.com>
2020-04-02 10:26:05 +0900

    苏做出了第一次上传

--- reptest/src/com/mmmp/lesson/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.mmmp.lesson;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("又是充满痛苦的一天啊!");
-    }
-}
---
+++ teamtest/src/tste01.java
@@ -0,0 +1,5 @@
+public class tste01 {
+    public static void main(String[] args) {
+        System.out.println("大家好我是一号队员");
+    }
+}
